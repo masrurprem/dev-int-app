b70d9788d9744820e5dea248dcf1bda5f34a2a07
masrurprem <masrur.kuet@gmail.com>
2025-08-10 14:34:39 +0600

    user and blog post models created

---
+++ src/models/postModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+//post schema
+const postSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  verdict: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  remarks: {
+    type: String,
+  },
+});
+
+//post model
+const postModel = new mongoose.model("postModel", postSchema);
+
+module.exports = postModel;
---
+++ src/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+//user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// user model
+const userModel = new mongoose.model("userModel", userSchema);
+
+module.exports = userModel;

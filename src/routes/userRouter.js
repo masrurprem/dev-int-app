2149aa5f83df8edf681fd12e63cb8147501110e4
masrurprem <masrur.kuet@gmail.com>
2025-08-11 20:35:57 +0600

    user logout route added

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,9 @@
 const userRoute = require("express").Router();
 const userModel = require("../models/userModel");
 const sendWelcomeMail = require("../email/userEmail");
+const bcrypt = require("bcryptjs");
+const auth = require("../middlewares/auth");
+require("dotenv").config();
 
 // user register route
 userRoute.post("/register", async (req, res) => {
@@ -14,6 +17,42 @@ userRoute.post("/register", async (req, res) => {
   } catch (err) {
     //console.log(err);
     res.status(400).send("unable to register..try again");
+  }
+});
+//user login route
+userRoute.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findByCredentials(email, password);
+    //generate user token
+    const token = await user.generateAuthToken();
+    res.status(200).send({ user, token });
+  } catch (e) {
+    //console.log(e);
+    res.status(404).send("login failed");
+  }
+});
+
+//user logout route
+userRoute.post("/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((tokenObj) => {
+      return tokenObj.token !== user.token;
+    });
+    await req.user.save();
+    res.status(200).send("You're successfully logged out!");
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+//all device user log out
+userRoute.post("/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.status(200).send("logged out from all devices");
+  } catch (err) {
+    res.status(404).send();
   }
 });
 
